b892d372b5994df0fcf73422b431d6990d321a62
yu-115 <malemaker@proton.me>
2024-09-26 00:27:25 +0900

    繰り返し処理課題を追加

---
+++ kadai_009/holiday.js
@@ -0,0 +1,16 @@
+const holidays = ['正月', '成人の日', '建国記念日', '天皇誕生日', '春分の日', '昭和の日', '憲法記念日', 'みどりの日',
+                  'こどもの日', '海の日', '山の日', '敬老の日', '秋分の日', 'スポーツの日', '文化の日', '勤労感謝の日'];
+
+//for文
+for (let i = 0; i <= 15; i++) {
+  console.log(holidays[i]);
+}
+
+//while文
+let i_2 = 0;
+console.log('ここから下はwhile文です');
+
+while (i_2 <= 15) {
+  console.log(holidays[i_2]);
+  i_2++; 
+}
