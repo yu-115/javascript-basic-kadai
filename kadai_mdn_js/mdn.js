64ac8af719bb7a4d681ed9046df95bbfb5401592
yu-115 <malemaker@proton.me>
2024-09-26 23:01:39 +0900

    MDN課題を追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const day = today.getDate();
+
+console.log(year + '年' + (month + 1) + '月' + day + '日');
